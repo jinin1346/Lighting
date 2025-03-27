871c75916442cecd09c3f7042f768a2be3693586
dnjs0 <heatherray@naver.com>
2025-03-27 16:52:46 +0900

    회원가입

--- src/main/java/com/lighting/login/Register.java
+++ src/main/java/com/lighting/login/Register.java
@@ -8,14 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.lighting.login.model.MemberDTO;
+
 @WebServlet("/login/register.do")
 
 public class Register extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //Register.java
         
+
 
         req.getRequestDispatcher("/WEB-INF/views/login/register.jsp").forward(req, resp);
 
---
+++ src/main/java/com/lighting/login/model/ActivityRegionDTO.java
@@ -0,0 +1,16 @@
+package com.lighting.login.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+
+public class ActivityRegionDTO {
+
+    private int tblMemberSeq;
+    private int tblActivityRegionCoordinateSeq;
+}
