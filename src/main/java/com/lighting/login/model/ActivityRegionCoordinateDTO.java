cbe877bd6f18d97ec56ee9d4e3ff6415a1d93d14
dnjs0 <heatherray@naver.com>
2025-03-27 09:57:34 +0900

    registerok

---
+++ src/main/java/com/lighting/login/model/ActivityRegionCoordinateDTO.java
@@ -0,0 +1,18 @@
+package com.lighting.login.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class ActivityRegionCoordinateDTO {
+
+    private String tblActivityRegionCoordinateSeq;
+    private String sido;
+    private String gugun;
+    private String latitude;
+    private String longitude;
+}
