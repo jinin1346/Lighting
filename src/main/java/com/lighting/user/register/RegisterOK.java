8d7fe00ed195ddf8a6e026c1a114c0bd823bb212
dnjs0 <heatherray@naver.com>
2025-03-28 17:52:36 +0900

    update?

--- src/main/java/com/lighting/user/register/RegisterOK.java
+++ src/main/java/com/lighting/user/register/RegisterOK.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login/registerok.do")
+@WebServlet("/user/register/registerok.do")
 public class RegisterOK extends HttpServlet {
 
     @Override
