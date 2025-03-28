7fe9fa4fadf338b084d1835ce33a7c2a023484e6
dnjs0 <heatherray@naver.com>
2025-03-29 00:04:55 +0900

    ajax

--- src/main/java/com/lighting/Main.java
+++ src/main/java/com/lighting/Main.java
@@ -1,6 +1,7 @@
 package com.lighting;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.lighting.model.MainDAO;
+import com.lighting.model.MainDTO;
+
 @WebServlet("/main.do")
 public class Main extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	    
+	    MainDAO dao = new MainDAO();
+        List<MainDTO> meetingList = dao.getMeetingList();
+        req.setAttribute("meetingList", meetingList);
+        
+        
 		req.getRequestDispatcher("/WEB-INF/views/main.jsp").forward(req, resp);
 	}  
 
