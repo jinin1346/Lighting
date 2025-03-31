0ef2aff86460d0069bfa4c14353d2eeaca503325
dnjs0 <heatherray@naver.com>
2025-03-31 12:44:30 +0900

    검색

--- src/main/java/com/lighting/Main.java
+++ src/main/java/com/lighting/Main.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.lighting.model.MainDAO;
 import com.lighting.model.MainDTO;
@@ -18,8 +17,7 @@ public class Main extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	    HttpSession session = req.getSession(true);
-        session.setAttribute("auth", 1); //인증티켓
+	    
 	    
 	    MainDAO dao = new MainDAO();
         List<MainDTO> meetingList = dao.getMeetingList();
